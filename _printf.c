4b35ad72d068718098adc24f9ae227ebbdde20be
Onahi7 <dicksonhardy7@gmail.com>
2023-08-19 21:41:59 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,66 +1,51 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+ * main - Test _printf
+ * Return: 0
+*/
+int main(void) {
+  _printf("Hello %c%s%c", 'A', "World", '!');
+  return 0;
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+ * _printf - Format and print output
+ * @format: Format string
+ * Return: Number of chars printed
+*/
+int _printf(const char *format, ...) {
+  
+  va_list args;
+  va_start(args, format);
 
-	*buff_ind = 0;
+  int chars_printed = 0;
+
+  if (!format) {
+    va_end(args);
+    return -1;
+  }
+
+  while (*format) {
+    if (*format == '%') {
+      format++;
+      switch (*format) {
+       case 'c':
+         chars_printed += write(1, va_arg(args, int), 1);
+         break;
+       case 's':
+         chars_printed += write(1, va_arg(args, char *), 
+           strlen(va_arg(args, char *)));
+         break;
+      }
+    } else {
+      chars_printed += write(1, format, 1); 
+    }
+    format++;
+  }
+  
+  va_end(args);
+  return chars_printed;
 }
