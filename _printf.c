1e48406e2a235b2c228d4131252de2f53aa1cd8c
Onahi7 <dicksonhardy7@gmail.com>
2023-08-19 21:53:39 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,51 +1,57 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+#include <string.h>
 
-/**
+/** 
  * main - Test _printf
  * Return: 0
 */
 int main(void) {
-  _printf("Hello %c%s%c", 'A', "World", '!');
-  return 0;
+    _printf("Hello %c%s%c", 'A', "World", '!');
+    return 0;
 }
 
 /**
- * _printf - Format and print output
+ * _printf - Format and print output 
  * @format: Format string
  * Return: Number of chars printed
 */
 int _printf(const char *format, ...) {
+
+    int chars_printed = 0;
   
-  va_list args;
-  va_start(args, format);
+    va_list args;
+    va_start(args, format);
 
-  int chars_printed = 0;
-
-  if (!format) {
-    va_end(args);
-    return -1;
-  }
-
-  while (*format) {
-    if (*format == '%') {
-      format++;
-      switch (*format) {
-       case 'c':
-         chars_printed += write(1, va_arg(args, int), 1);
-         break;
-       case 's':
-         chars_printed += write(1, va_arg(args, char *), 
-           strlen(va_arg(args, char *)));
-         break;
-      }
-    } else {
-      chars_printed += write(1, format, 1); 
+    if (!format) {
+        va_end(args);
+        return -1; 
     }
-    format++;
-  }
-  
-  va_end(args);
-  return chars_printed;
+
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    {
+                        char c = (char)va_arg(args, int);
+                        chars_printed += write(1, &c, 1); 
+                    }
+                    break;
+                case 's':
+                    {
+                        char *str = va_arg(args, char*);
+                        chars_printed += write(1, str, strlen(str));
+                    }
+                    break;
+            }
+        } else {
+            chars_printed += write(1, format, 1);
+        }
+        format++;
+    }
+
+    va_end(args);
+    return chars_printed;
 }
