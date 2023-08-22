e2417d65e500f7f3681d46acda72bd6576f15aef
Enosuji <enoosuji@gmail.com>
2023-08-22 23:05:21 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -17,6 +17,7 @@ int is_printable(char c)
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
+ * Return: Number of chars printed
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
